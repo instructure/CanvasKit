2da98d543a435063c7be53bd762fc431d17e27a5
Derrick J. Hathaway <derrick@instructure.com>
2013-12-04 14:37:13 -0700

    adds attachment ids to createMessage:

--- CanvasKit/Networking/CKIClient+CKIConversation.h
+++ CanvasKit/Networking/CKIClient+CKIConversation.h
@@ -19,8 +19,15 @@ typedef NS_ENUM(NSInteger, CKIConversationScope) {
 @interface CKIClient (CKIConversation)
 - (RACSignal *)fetchConversationsInScope:(CKIConversationScope)scope;
 
+- (RACSignal *)refreshConversation:(CKIConversation *)conversation;
+
 /**
  on success the signal will send a single CKIConversation object (pretty sure)
  */
 - (RACSignal *)createConversationWithRecipientIDs:(NSArray *)recipients message:(NSString *)message;
+
+/**
+ posts message to the given conversation with the attachment IDs provided
+ */
+- (RACSignal *)createMessage:(NSString *)message inConversation:(CKIConversation *)conversation withAttachmentIDs:(NSArray *)attachments;
 @end
