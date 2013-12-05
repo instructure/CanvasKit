5df5be19ffe6a36086a0f2b627749bb71c8930be
Derrick J. Hathaway <derr@me.com>
2013-12-05 15:51:54 -0700

    Sets the correct context on assignments for todo items

--- CanvasKit/Networking/CKIClient+CKITodoItem.m
+++ CanvasKit/Networking/CKIClient+CKITodoItem.m
@@ -9,19 +9,32 @@
 #import "CKIClient+CKITodoItem.h"
 #import "CKITodoItem.h"
 #import "CKICourse.h"
+#import "CKIAssignment.h"
+#import <ReactiveCocoa/ReactiveCocoa.h>
 
 @implementation CKIClient (CKITodoItem)
 
 - (RACSignal *)fetchTodoItemsForCourse:(CKICourse *)course
 {
     NSString *path = [[course path] stringByAppendingPathComponent:@"todo"];
-    return [self fetchResponseAtPath:path parameters:nil modelClass:[CKITodoItem class] context:course];
+    return [[self fetchResponseAtPath:path parameters:nil modelClass:[CKITodoItem class] context:course] map:^(NSArray  *value) {
+        for (CKITodoItem *item in value) {
+            item.assignment.context = course;
+        }
+        return value;
+    }];
+    
 }
 
 - (RACSignal *)fetchTodoItemsForCurrentUser
 {
     NSString *path = [CKIRootContext.path stringByAppendingPathComponent:@"users/self/todo"];
-    return [self fetchResponseAtPath:path parameters:nil modelClass:[CKITodoItem class] context:nil];
+    return [[self fetchResponseAtPath:path parameters:nil modelClass:[CKITodoItem class] context:nil] map:^(NSArray  *value) {
+        for (CKITodoItem *item in value) {
+            item.assignment.context = [CKICourse modelWithID:item.assignment.courseID];
+        }
+        return value;
+    }];
 }
 
 @end
