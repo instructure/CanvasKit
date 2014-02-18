181c2fe2ae5ee9025d4da0e1514dd9f15909380f
Derrick J. Hathaway <derr@me.com>
2014-02-18 14:07:09 -0800

    Adds back in the root context fetch method for stream items

--- CanvasKit/Networking/CKIClient+CKIActivityStreamItem.h
+++ CanvasKit/Networking/CKIClient+CKIActivityStreamItem.h
@@ -13,10 +13,18 @@
 
 @interface CKIClient (CKIActivityStreamItem)
 
+
+/**
+ Fetches the activity stream for the current user.
+ 
+ @return A signal that will deliver pages of stream items.
+ */
+- (RACSignal *)fetchActivityStream;
+
 /**
  Fetches the activity stream for the given context.
  
- @param context the context for the stream
+ @param context the context for the stream i.e. a course
  @return a signal of pages (NSArray *) of stream items.
  */
 - (RACSignal *)fetchActivityStreamForContext:(id<CKIContext>)context;
