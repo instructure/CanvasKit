5254619c61bd99aa0c140039948fbb92b16a0072
Nathan Lambson <nlambson@instructure.com>
2014-03-24 09:13:25 -0600

    Fix for fetchCalendarEventsForContext

--- CanvasKit/Networking/CKIClient+CKICalendarEvent.m
+++ CanvasKit/Networking/CKIClient+CKICalendarEvent.m
@@ -32,12 +32,12 @@
     NSString *path = [CKIRootContext.path stringByAppendingPathComponent:@"calendar_events"];
     
     NSString *contextCode = @"";
-    if ([[context path] rangeOfString:@"courses"].location != NSNotFound){
-        contextCode = [NSString stringWithFormat:@"course_%@", context];
-    } else if ([[context path] rangeOfString:@"groups"].location != NSNotFound){
-        contextCode = [NSString stringWithFormat:@"groups_%@", context];
-    } else if ([[context path] rangeOfString:@"users"].location != NSNotFound) {
-        contextCode = [NSString stringWithFormat:@"users_%@", context];
+    if ([context isKindOfClass:[CKICourse class]]){
+        contextCode = [NSString stringWithFormat:@"course_%@", ((CKICourse *)context).id];
+    } else if ([context isKindOfClass:[CKIGroup class]]){
+        contextCode = [NSString stringWithFormat:@"groups_%@", ((CKIGroup *)context).id];
+    } else if ([context isKindOfClass:[CKIUser class]]) {
+        contextCode = [NSString stringWithFormat:@"users_%@", ((CKIUser *)context).id];
     }
     
     NSDictionary *params = @{@"type": @"event",
