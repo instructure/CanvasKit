a03860056e8097d0bff92dc53f96b2452ba9a6d4
Nathan Lambson <nlambson@instructure.com>
2014-05-21 15:52:11 -0600

    Added support for basic HTTP authentication

--- CanvasKit/UIKit+CanvasKit/CKILoginViewController.m
+++ CanvasKit/UIKit+CanvasKit/CKILoginViewController.m
@@ -9,8 +9,9 @@
 #import "CKILoginViewController.h"
 #import "NSString+CKIAdditions.h"
 
-@interface CKILoginViewController () <UIWebViewDelegate>
+@interface CKILoginViewController () <UIWebViewDelegate, NSURLSessionTaskDelegate, UIAlertViewDelegate>
 @property (nonatomic, copy) NSURLRequest *request;
+@property (nonatomic, copy) void(^completionHandler)(NSURLSessionAuthChallengeDisposition, NSURLCredential *);
 @end
 
 @implementation CKILoginViewController
@@ -35,13 +36,78 @@
     [self.webView setBackgroundColor:[UIColor blackColor]];
     self.view = self.webView;
     
-    [self.webView loadRequest:self.request];
+    NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:config delegate:self delegateQueue:nil];
+    [[session dataTaskWithURL:[self.request URL]
+            completionHandler:^(NSData *data,
+                                NSURLResponse *response,
+                                NSError *error) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [self.webView loadRequest:self.request];
+                });
+            }] resume];
+}
+
+#pragma mark - UIALertView Delegate
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex == 1) {
+        [self cancelOAuth];
+    } else {
+        NSString *username = [[alertView textFieldAtIndex:0] text];
+        NSString *password = [[alertView textFieldAtIndex:1] text];
+        
+        NSURLCredential *secretHandshake = [NSURLCredential credentialWithUser:username password:password persistence:NSURLCredentialPersistenceForSession];
+        if (self.completionHandler) {
+            self.completionHandler(NSURLSessionAuthChallengeUseCredential,secretHandshake);
+        }
+    }
+}
+
+#pragma mark - NSURLSession Delegate
+
+- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didReceiveChallenge:(NSURLAuthenticationChallenge *)challenge completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition, NSURLCredential *))completionHandler
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSURLSessionAuthChallengeDisposition disposition = NSURLSessionAuthChallengePerformDefaultHandling;
+        NSURLCredential *credential = nil;
+        self.completionHandler = completionHandler;
+        
+        if ([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodNTLM] || [challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodHTTPBasic]){
+            UIAlertView *alertView = [UIAlertView new];
+            alertView.delegate = self;
+            alertView.title = @"Login";
+            alertView.alertViewStyle = UIAlertViewStyleLoginAndPasswordInput;
+            [alertView textFieldAtIndex:0].placeholder = @"Username";
+            [alertView addButtonWithTitle:@"OK"];
+            [alertView addButtonWithTitle:@"Cancel"];
+            
+            [alertView show];
+        } else if ([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust]){
+            //See AFURLSessionManager
+            credential = [NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust];
+            disposition = NSURLSessionAuthChallengeUseCredential;
+            
+            if (self.completionHandler) {
+                self.completionHandler(disposition, credential);
+            }
+        } else {
+            if (self.completionHandler) {
+                self.completionHandler(disposition, credential);
+            }
+        }
+    });
 }
 
 #pragma mark - Webview Delegate
 
 - (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
 {
+    if ([[[request URL] description] isEqualToString:@"about:blank"]) {
+        return NO;
+    }
+    
     if ([self getValueFromRequest:request withKey:@"code"]  ) {
         self.successBlock([self getValueFromRequest:request withKey:@"code"]);
         return NO;
@@ -75,7 +141,5 @@
         self.failureBlock([NSError errorWithDomain:@"com.instructure.canvaskit" code:0 userInfo:@{NSLocalizedDescriptionKey: @"User cancelled authentication"}]);
     }];
 }
-
-
 
 @end
